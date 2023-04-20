f6d54c51a1161b12a1700b0f668c9c341ffc0abc
ashyadav002 <53461787+ashyadav002@users.noreply.github.com>
2023-04-20 08:12:35 +0530

    Update urls.py

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello_world/', include('hello_world.urls')),
-    path('polls/', include('polls.urls'))
+    path('gcloud_test/', include('hello_world.urls')),
+    path('gcloud_test_poll/', include('polls.urls'))
 ]
